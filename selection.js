3034766a894836385cddca47fbdee19a04ebbb6b
Ricardo <ricardoplouis@gmail.com>
2018-01-07 19:06:58 -0600

    Refactored for readability

--- selection.js
+++ selection.js
@@ -1,5 +1,5 @@
 document.addEventListener('mouseup', (event) => {
-  let selection = returnSelectedText().toString()
+  var selection = returnSelectedText().toString()
   highlightSelection()
   console.log('returnSelectedText', returnSelectedText());
 });
@@ -11,73 +11,80 @@ function returnSelectedText(){
 //highlight copied from https://stackoverflow.com/questions/304837/javascript-user-selection-highlighting
 //refactor and reread to understand it more later
 
-function getSafeRanges(dangerous) {
-    var a = dangerous.commonAncestorContainer;
-    // Starts -- Work inward from the start, selecting the largest safe range
-    var s = new Array(0), rs = new Array(0);
-    if (dangerous.startContainer != a)
-        for(var i = dangerous.startContainer; i != a; i = i.parentNode)
-            s.push(i)
-    ;
-    if (0 < s.length) for(var i = 0; i < s.length; i++) {
-        var xs = document.createRange();
-        if (i) {
-            xs.setStartAfter(s[i-1]);
-            xs.setEndAfter(s[i].lastChild);
-        }
-        else {
-            xs.setStart(s[i], dangerous.startOffset);
-            xs.setEndAfter(
-                (s[i].nodeType == Node.TEXT_NODE)
-                ? s[i] : s[i].lastChild
-            );
-        }
-        rs.push(xs);
+function getSafeRanges(dangerRange) {
+  var commonContainer = dangerRange.commonAncestorContainer;
+  // Starts -- Work inward from the start, selecting the largest safe range
+  var s = new Array(0), rs = new Array(0);
+  if (dangerRange.startContainer != commonContainer){
+    for(var i = dangerRange.startContainer; i != commonContainer; i = i.parentNode){
+      s.push(i)
     }
+  }
+  if (0 < s.length){
+    for(var i = 0; i < s.length; i++) {
+      var xs = document.createRange();
+      if (i){
+        xs.setStartAfter(s[i-1]);
+        xs.setEndAfter(s[i].lastChild);
+      }
+      else {
+        xs.setStart(s[i], dangerRange.startOffset);
+        xs.setEndAfter(
+          (s[i].nodeType == Node.TEXT_NODE)
+          ? s[i] : s[i].lastChild
+        );
+      }
+      rs.push(xs);
+    }
+  }
 
-    // Ends -- basically the same code reversed
-    var e = new Array(0), re = new Array(0);
-    if (dangerous.endContainer != a)
-        for(var i = dangerous.endContainer; i != a; i = i.parentNode)
-            e.push(i)
-    ;
-    if (0 < e.length) for(var i = 0; i < e.length; i++) {
-        var xe = document.createRange();
+  // Ends -- basically the same code reversed
+  var e = new Array(0), re = new Array(0);
+  if (dangerRange.endContainer != commonContainer){
+    for(var i = dangerRange.endContainer; i != commonContainer; i = i.parentNode){
+      e.push(i)
+    }
+  }
+
+  if (0 < e.length){
+    for(var i = 0; i < e.length; i++) {
+        var docRange = document.createRange();
         if (i) {
-            xe.setStartBefore(e[i].firstChild);
-            xe.setEndBefore(e[i-1]);
-        }
-        else {
-            xe.setStartBefore(
+            docRange.setStartBefore(e[i].firstChild);
+            docRange.setEndBefore(e[i-1]);
+        } else {
+            docRange.setStartBefore(
                 (e[i].nodeType == Node.TEXT_NODE)
                 ? e[i] : e[i].firstChild
             );
-            xe.setEnd(e[i], dangerous.endOffset);
+            docRange.setEnd(e[i], dangerRange.endOffset);
         }
-        re.unshift(xe);
+        re.unshift(docRange);
     }
+  }
+  // Middle -- the uncaptured middle
+  if ((0 < s.length) && (0 < e.length)) {
+      var xm = document.createRange();
+      xm.setStartAfter(s[s.length - 1]);
+      xm.setEndBefore(e[e.length - 1]);
+  }
+  else {
+      return [dangerRange];
+  }
 
-    // Middle -- the uncaptured middle
-    if ((0 < s.length) && (0 < e.length)) {
-        var xm = document.createRange();
-        xm.setStartAfter(s[s.length - 1]);
-        xm.setEndBefore(e[e.length - 1]);
-    }
-    else {
-        return [dangerous];
-    }
+  // Concat
+  rs.push(xm);
+  response = rs.concat(re);
 
-    // Concat
-    rs.push(xm);
-    response = rs.concat(re);
-
-    // Send to Console
-    return response;
+  // Send to Console
+  return response;
 }
 
 function highlightSelection() {
-    var userSelection = window.getSelection().getRangeAt(0);
-    var safeRanges = getSafeRanges(userSelection);
+    var userSelection = window.getSelection().getRangeAt(0),
+        safeRanges = getSafeRanges(userSelection);
+
+    console.log('safeRanges', safeRanges);
     for (var i = 0; i < safeRanges.length; i++) {
         highlightRange(safeRanges[i]);
     }
